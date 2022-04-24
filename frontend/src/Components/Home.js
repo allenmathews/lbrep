4295ecf76bc0d59a81d2033faec4043cd25a38ea
Allen Mathews <allenjoemathews@gmail.com>
2022-04-25 08:30:09 +1000

    added mui

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,16 +1,10 @@
 import React from 'react'
+import { Button } from '@mui/material';
 import {Link} from 'react-router-dom';
 
 function Home() {
     return ( 
-        <div >
-        <h1> This is the homepage </h1> 
-        <br />
-        <Link to="/login">Login</Link>
-        <br />
-        <Link to='/listings'>Listings</Link>
-
-        </div>
+        <Button variant="text">Text</Button>
     )
 }
 
