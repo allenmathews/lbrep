1d244d3e1f406e93f11fc2be2addaf60da63b55c
Allen Mathews <allenjoemathews@gmail.com>
2022-05-01 15:21:57 +1000

    built the home page

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,19 +1,68 @@
 import React, {useState} from 'react'
-import { Button, Typography, Grid } from '@mui/material';
+import { Button, Typography, Grid, AppBar, Toolbar } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import {Link} from 'react-router-dom';
 import CustomCard from './CustomCard';
 import CssBaseline from '@mui/material/CssBaseline';
-
+import city from './Assets/city.jpg'
 
 const useStyles = makeStyles({
-    divStyle: {
-        width: "100%", border: "2px solid red", padding:"15px"
-    },
+  leftNav: {
+      marginRight: 'auto'
+  },
+  rightNav: {
+      marginLeft: 'auto',
+      marginRight: '10rem'
+  },
 
-    btnStyle: {
-        backgroundColor: "yellow",
+  propertyBtn: {
+      backgroundColor: 'green',
+      color: 'white',
+      width: "15rem",
+      fontSize: "1.1rem",
+      marginRight: "1rem",
+      '&:hover': {
+        backgroundColor: 'blue',
     }
+  },
+
+  loginBtn: {
+      backgroundColor: 'white',
+      color: 'black',
+      width: "15rem",
+      fontSize: "1.1rem",
+      marginLeft: "1rem",
+      '&:hover': {
+          backgroundColor: 'green',
+      }
+  },
+
+  cityImg: {
+      width: '100%',
+      height: '92vh'
+  },
+
+  overlayText: {
+    position: 'absolute',
+    zIndex: "100",
+    top: '100px',
+    left: '20px',
+    textAlign: 'center',
+  },
+
+  homeText:{
+    color: 'white',
+    fontWeight: 'bolder',
+
+  },
+
+  homeBtn:{
+      fontSize: '3.5rem',
+      borderRadius: '15px',
+      backgroundColor: 'green',
+      marginTop: '2rem',
+      boxShadow: '3px 3px 3px white',
+  }
 })
 
 function Home() {
@@ -22,26 +71,41 @@ function Home() {
     return (
       <>
       <CssBaseline />
-      <Grid 
-      direction="column"
-      justifyContent="space-evenly" alignItems="flex-start" container style={{height: '100vh', backgroundColor: 'grey'}}>
-
-
-        <Grid item container>
-        <Grid item xs={5}>
-        <CustomCard />
-        </Grid>
-        </Grid>
-        <Grid item container>
-        <Grid item xs={5}>
-        <CustomCard />
-        </Grid>
-        </Grid>
-
-        
-        
-       
-        </Grid>
+      <AppBar position="static" style={{backgroundColor: 'black' }} >
+        <Toolbar>
+            <div className={classes.leftNav}>
+            <Button color="inherit">
+                <Typography variant="h4">LBREP</Typography>{" "}
+            </Button>
+            </div>
+            <div>
+            <Button color="inherit" style={{marginRight: '2rem'}}>
+                <Typography variant='h6'>Listings</Typography>{" "}
+                </Button>
+          <Button color="inherit" style={{marginLeft: '2rem'}}>
+          <Typography variant='h6'>Agencies</Typography>{" "}
+              </Button>
+            </div>
+            <div className={classes.rightNav}>
+            <Button  className={classes.propertyBtn}>
+            <Typography>Add Property</Typography>
+            </Button>
+          <Button  className={classes.loginBtn}>
+          <Typography>Login</Typography>
+              </Button>
+            </div>
+          
+          
+        </Toolbar>
+      </AppBar>
+      <div style={{position: 'relative'}}>
+      <img src={city} className={classes.cityImg} />
+      <div className={classes.overlayText}>
+          <Typography variant="h1" className={classes.homeText}>FIND YOUR <span style={{color: 'green'}}>NEXT PROPERTY</span> ON THE LBREP WEBSITE</Typography>
+          <Button variant="contained" className={classes.homeBtn}>SEE ALL PROPERTIES</Button>
+      </div>
+      </div>
+      
       </>
   );
 }
