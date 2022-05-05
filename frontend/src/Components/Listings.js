a4dcf9b0266dc0583e2cf4c8d3f7cc4eb4cdb66a
Allen Mathews <allenjoemathews@gmail.com>
2022-05-05 22:05:56 +1000

    added library

--- frontend/src/Components/Listings.js
+++ frontend/src/Components/Listings.js
@@ -5,14 +5,17 @@ import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
 //MUI
 import {Grid, AppBar, Typography} from '@mui/material';
 
+//Map icons
+import houseIconPng from './Assets/Mapicons/houseonIcPng'
+import apartmentIconPng from './Assets/Mapicons/apartmentIconPng'
+import houseIconPng from './Assets/Mapicons/houseIconPng'
+
 function Listings() {
     return ( 
       <Grid container>
         <Grid item xs={4}>
           <Typography variant='h1'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur repellat voluptate laboriosam reprehenderit culpa similique illo autem est assumenda sed. Reprehenderit eos eius corrupti voluptate autem placeat esse dolorem ea optio doloribus, nostrum reiciendis, nemo ullam, eligendi obcaecati vitae tempora. Beatae quisquam, sint eum eaque, necessitatibus quam, facere illum commodi impedit natus tempore ratione debitis rem non cupiditate ea tenetur unde. Voluptates dicta magnam, ab rem eos, maxime neque veritatis similique ut suscipit explicabo voluptatum exercitationem cupiditate possimus nesciunt totam ipsum praesentium aspernatur. Ad iste non ipsam. Explicabo, mollitia iure quaerat, recusandae necessitatibus praesentium cumque, voluptatem excepturi saepe magni tempora.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur repellat voluptate laboriosam reprehenderit culpa similique illo autem est assumenda sed. Reprehenderit eos eius corrupti voluptate autem placeat esse dolorem ea optio doloribus, nostrum reiciendis, nemo ullam, eligendi obcaecati vitae tempora. Beatae quisquam, sint eum eaque, necessitatibus quam, facere illum commodi impedit natus tempore ratione debitis rem non cupiditate ea tenetur unde. Voluptates dicta magnam, ab rem eos, maxime neque veritatis similique ut suscipit explicabo voluptatum exercitationem cupiditate possimus nesciunt totam ipsum praesentium aspernatur. Ad iste non ipsam. Explicabo, mollitia iure quaerat, recusandae necessitatibus praesentium cumque, voluptatem excepturi saepe magni tempora.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur repellat voluptate laboriosam reprehenderit culpa similique illo autem est assumenda sed. Reprehenderit eos eius corrupti voluptate autem placeat esse dolorem ea optio doloribus, nostrum reiciendis, nemo ullam, eligendi obcaecati vitae tempora. Beatae quisquam, sint eum eaque, necessitatibus quam, facere illum commodi impedit natus tempore ratione debitis rem non cupiditate ea tenetur unde. Voluptates dicta magnam, ab rem eos, maxime neque veritatis similique ut suscipit explicabo voluptatum exercitationem cupiditate possimus nesciunt totam ipsum praesentium aspernatur. Ad iste non ipsam. Explicabo, mollitia iure quaerat, recusandae necessitatibus praesentium cumque, voluptatem excepturi saepe magni tempora.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda qui libero optio vero magnam illum natus esse asperiores ipsam, tempora nesciunt commodi enim nobis provident repellendus iure voluptate mollitia! Iste commodi aut quas qui mollitia perferendis, aspernatur soluta cupiditate fugiat saepe recusandae ad magnam officia debitis aliquam quidem cumque, nisi quaerat ipsa voluptatem facilis non ex consectetur autem. Quis blanditiis mollitia magnam repellat? Fugit fuga earum ab explicabo iure? Porro impedit accusamus suscipit debitis dolorum, atque tempore non ducimus repudiandae! Sequi perspiciatis quas facere officiis saepe praesentium aperiam, suscipit aut illum nostrum, vel non accusantium, cupiditate placeat quam ut deserunt.
             </Typography>          
           BLANK SPACE
         </Grid>
